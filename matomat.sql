41a7993d7fb05b07ce97cdca90bf18594ec9cb52
jm <jo@jonasmai.de>
2017-11-21 18:06:03 +0100

    removed c++/qt code changed database init script to support sqlite

--- matomat.sql
+++ matomat.sql
@@ -1,37 +1,48 @@
 
-
+-- Product table
 DROP TABLE IF EXISTS Products;
 CREATE TABLE Products
 (
-   ID int PRIMARY KEY,
-   Name varchar(25),
-   Price int
-); 
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT NOT NULL,
+    price REAL NOT NULL
+);
+CREATE UNIQUE INDEX Products_id_uindex ON Products (id);
+CREATE UNIQUE INDEX Products_name_uindex ON Products (name);
 
+-- User table
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users
 (
-   ID int PRIMARY KEY,
-   Name varchar(25),
-   NFC_Id char(25)
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT,
+    card_id INTEGER NOT NULL
 );
+CREATE UNIQUE INDEX Users_id_uindex ON Users (id);
+CREATE UNIQUE INDEX Users_card_id_uindex ON Users (card_id);
+INSERT INTO Users (name, card_id) VALUES ('Fachschaft', 0);
 
-INSERT INTO Users (ID, Name, NFC_Id) VALUES (0, 'fachschaft', '');
-
+-- Transaction table
 DROP TABLE IF EXISTS Transactions;
 CREATE TABLE Transactions
 (
-   ID int PRIMARY KEY,
-   From_user int,
-   To_user int,
-   amount int,
-   timestamp DATETIME
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "from" INTEGER NOT NULL,
+    "to" INTEGER NOT NULL,
+    timestamp TEXT NOT NULL,
+    CONSTRAINT transactions_Users_id_fk FOREIGN KEY ("from") REFERENCES Users (id) ON UPDATE CASCADE,
+    CONSTRAINT transactions_Users_id_fk FOREIGN KEY ("to") REFERENCES Users (id) ON UPDATE CASCADE
 );
+CREATE UNIQUE INDEX transactions_id_uindex ON transactions (id);
 
-DROP TABLE IF EXISTS TransactionItems;
-CREATE TABLE TransactionItems
+-- Items to transactions table
+DROP TABLE IF EXISTS Transaction_Products;
+CREATE TABLE Transaction_Products
 (
-   ID int PRIMARY KEY,
-   Transaction int,
-   Product int
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "transaction" INTEGER NOT NULL,
+    product INTEGER NOT NULL,
+    CONSTRAINT Transaction_Products_Transactions_id_fk FOREIGN KEY ("transaction") REFERENCES Transactions (id),
+    CONSTRAINT Transaction_Products_Products_id_fk FOREIGN KEY (product) REFERENCES Products (id)
 );
+CREATE UNIQUE INDEX Transaction_Products_id_uindex ON Transaction_Products (id);
